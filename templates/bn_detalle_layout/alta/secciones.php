2e3ed9237bd0643242fd485d37a05ed45f0655d3
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-18 13:58:49 -0500

    v0.13.0 Se instegra CRUD bn_detalle_layout con sus estilos y vistas

---
+++ templates/bn_detalle_layout/alta/secciones.php
@@ -0,0 +1,10 @@
+<div class="col-md-3 secciones">
+
+    <div class="col-md-12 int_secciones ">
+        <div class="col-md-8">
+            <h3>Alta de Detalle Layout</h3>
+            <?php include "templates/bn_detalle_layout/_base/buttons/1.azul.php"; ?>
+        </div>
+
+    </div>
+</div>
---
+++ templates/bn_detalle_layout/lista/secciones.php
@@ -0,0 +1,10 @@
+<div class="col-md-3 secciones">
+
+    <div class="col-md-12 int_secciones ">
+
+        <div class="col-md-8">
+            <h3>Detalle Layout</h3>
+            <?php include "templates/bn_detalle_layout/_base/links/alta.php"; ?>
+        </div>
+    </div>
+</div>
