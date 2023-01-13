daaf580c8bd9b6bc99470e60c5d75c94b629b15a
[user] <angel.martinez@ivitec.com.mx>
2023-01-13 16:08:13 -0600

    v0.21.3 Se integra sucursales

--- controllers/controlador_adm_session.php
+++ controllers/controlador_adm_session.php
@@ -21,6 +21,7 @@ class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_
     public string $link_lista_bn_banco = '';
     public string $link_lista_bn_tipo_banco = '';
     public string $link_lista_bn_tipo_sucursal = '';
+    public string $link_lista_bn_sucursal = '';
 
     /**
      * Funcion de controlador donde se ejecutaran siempre que haya un acceso denegado
@@ -70,6 +71,9 @@ class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_
 
         $hd = "index.php?seccion=bn_tipo_sucursal&accion=lista&session_id=$this->session_id";
         $this->link_lista_bn_tipo_sucursal = $hd;
+
+        $hd = "index.php?seccion=bn_sucursal&accion=lista&session_id=$this->session_id";
+        $this->link_lista_bn_sucursal = $hd;
 
         $this->include_menu = (new generales())->path_base;
         $this->include_menu .= 'templates/inicio.php';
