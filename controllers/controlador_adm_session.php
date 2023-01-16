73bfd1b332e852928e6b606d5eec336f13f8023f
[user] <angel.martinez@ivitec.com.mx>
2023-01-16 15:14:44 -0600

    v0.23.3 Se integra tipo cuenta

--- controllers/controlador_adm_session.php
+++ controllers/controlador_adm_session.php
@@ -22,6 +22,7 @@ class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_
     public string $link_lista_bn_tipo_banco = '';
     public string $link_lista_bn_tipo_sucursal = '';
     public string $link_lista_bn_sucursal = '';
+    public string $link_lista_bn_tipo_cuenta = '';
 
     /**
      * Funcion de controlador donde se ejecutaran siempre que haya un acceso denegado
@@ -74,6 +75,9 @@ class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_
 
         $hd = "index.php?seccion=bn_sucursal&accion=lista&session_id=$this->session_id";
         $this->link_lista_bn_sucursal = $hd;
+
+        $hd = "index.php?seccion=bn_tipo_cuenta&accion=lista&session_id=$this->session_id";
+        $this->link_lista_bn_tipo_cuenta = $hd;
 
         $this->include_menu = (new generales())->path_base;
         $this->include_menu .= 'templates/inicio.php';
---
+++ views/bn_tipo_cuenta/cuentas.php
@@ -0,0 +1,45 @@
+<?php /** @var tglobally\tg_banco\controllers\controlador_bn_tipo_cuenta $controlador */ ?>
+
+
+<?php (new \tglobally\template_tg\template())->sidebar($controlador); ?>
+
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+
+        <div class="  form-main" id="form">
+            <form method="post" action="<?php echo $controlador->link_bn_cuenta_alta_bd;?>" class="form-additional">
+
+                <?php echo $controlador->inputs->bn_cuenta_descripcion; ?>
+                <?php echo $controlador->inputs->select->bn_tipo_cuenta_id; ?>
+                <?php echo $controlador->inputs->select->org_sucursal_id; ?>
+                <?php echo $controlador->inputs->select->bn_empleado_id; ?>
+                <?php echo $controlador->inputs->select->bn_sucursal_id; ?>
+
+                <div class="buttons col-md-12">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="cuentas">Guarda</button>
+                    </div>
+                    <div class="col-md-6 ">
+                        <a href="<?php echo $controlador->link_lista; ?>"  class="btn btn-info btn-guarda col-md-12 ">Lista</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="lista">
+            <div class="card">
+                <div class="card-header">
+                    <span class="text-header">Bancos</span>
+                </div>
+                <div class="card-body">
+                    <div class="cont_tabla_sucursal  col-md-12">
+                        <?php echo $controlador->contenido_table; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
